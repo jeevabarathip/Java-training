294c8dfb333a21ce25acb3cbe8f684c3ecc0e626
jeevabarathip <94976002+jeevabarathip@users.noreply.github.com>
2023-07-12 21:02:45 +0530

    Add files via upload

---
+++ Java training/Day 3/factorial.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+ public int factorial(int input1){
+  int fact=1;
+for(int i=1;i<=input1;i++)
+ {
+fact=fact*i;
+ }
+return fact;
+ }
+}
